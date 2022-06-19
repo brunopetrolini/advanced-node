5a3a12f1749e1e8470531df1b9c4b9aa59a7d893
Bruno Petrolini <bruno.petrolini@contasimples.com>
2022-06-19 15:51:09 -0300

    isolate sut creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -11,13 +11,24 @@ class AxiosHttpClient {
 }
 
 describe('Axios Http Client', () => {
+  let sut: AxiosHttpClient;
+  let fakeAxios: jest.Mocked<typeof axios>;
+
+  const url = 'any_url';
+  const params = { any: 'any' };
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+  });
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient();
+  });
+
   it('should call get with correct params', async () => {
-    const fakeAxios = axios as jest.Mocked<typeof axios>;
-    const sut = new AxiosHttpClient();
+    await sut.get({ url, params });
 
-    await sut.get({ url: 'any_url', params: { any: 'any' } });
-
-    expect(fakeAxios.get).toHaveBeenCalledWith('any_url', { params: { any: 'any' } });
+    expect(fakeAxios.get).toHaveBeenCalledWith(url, { params });
     expect(fakeAxios.get).toHaveBeenCalledTimes(1);
   });
 });
